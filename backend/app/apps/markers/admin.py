7fdde2ab225125c2deca3e814dbe2af874f3a7d6
PervDaniil <studentdan123.900@gmail.com>
2025-02-25 21:46:29 +0600

    [Backend] - Registers in Admin dashboard for models in apps/markers

--- backend/app/apps/markers/admin.py
+++ backend/app/apps/markers/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Room, Marker, Meetup, MeetupPhoto
+
+
+admin.site.register(Room)
+admin.site.register(Marker)
+admin.site.register(Meetup)
+admin.site.register(MeetupPhoto)
